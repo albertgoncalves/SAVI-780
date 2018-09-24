f31cb8be9fbabaad360006bddbc5ca474ab72434
albertgoncalves <goncalves.albert@gmail.com>
2018-09-24 08:49:14 -0400

    Should be a bit more clear now.

--- hw4/script.ts
+++ hw4/script.ts
@@ -5,54 +5,56 @@ declare var L: any;
 // via https://gist.github.com/mjackson/5311256
 
 const hslToRgb = (h: number, s: number, l: number): number[] => {
+
+    const hslHelper = (hh: number, ss: number, ll: number): number[] => {
+        const q = ll < 0.5 ? ll * (1 + ss)
+                           : ll + ss - ll * ss;
+        const p = 2 * ll - q;
+        const rgb: number[][] = [ [p, q, hh + (1 / 3)] // r
+                                , [p, q, hh          ] // g
+                                , [p, q, hh - (1 / 3)] // b
+                                ];
+        return rgb.map((x) => hueToRgb(x) * 255);
+    };
+
+    const hueToRgb = (pqt: number[]): number => {
+        const [p, q, t] = pqt;
+        const tt = t < 0 ? t + 1
+                 : t > 1 ? t - 1
+                         : t;
+        return tt < (1 / 6) ? p + (q - p) * 6 * tt
+             : tt < (1 / 2) ? q
+             : tt < (2 / 3) ? p + (q - p) * ((2 / 3) - tt) * 6
+                            : p;
+    };
+
     return s === 0 ? [l, l, l]
                    : hslHelper(h, s, l);
 };
 
-const hue2rgb = (pqt: number[]): number => {
-    const [p, q, t] = pqt;
-    const tt = t < 0 ? t + 1
-             : t > 1 ? t - 1
-                     : t;
-    return tt < (1 / 6) ? p + (q - p) * 6 * tt
-         : tt < (1 / 2) ? q
-         : tt < (2 / 3) ? p + (q - p) * ((2 / 3) - tt) * 6
-                        : p;
-};
+const rgbToHex = (r: number, g: number, b: number): string => {
 
-const hslHelper = (hh: number, ss: number, ll: number): number[] => {
-    const q = ll < 0.5 ? ll * (1 + ss)
-                       : ll + ss - ll * ss;
-    const p = 2 * ll - q;
-    const rgb: number[][] = [ [p, q, hh + (1 / 3)] // r
-                            , [p, q, hh          ] // g
-                            , [p, q, hh - (1 / 3)] // b
-                            ];
-    return rgb.map((x) => hue2rgb(x) * 255);
-};
+    const rgbHelper = (rgb: number): string => {
+        const  hex = Number(rgb).toString(16);
+        return hex.length < 2 ? "0" + hex
+                              : hex;
+    };
 
-const rgbToHex = (rgb: number): string => {
-    const  hex = Number(rgb).toString(16);
-    return hex.length < 2 ? "0" + hex
-                          : hex;
-};
-
-const fullColorHex = (r: number, g: number, b: number): string => {
-    const [red, green, blue] = [r, g, b].map((x) => rgbToHex(x));
+    const [red, green, blue] = [r, g, b].map((x) => rgbHelper(x));
     return "#" + red + green + blue;
 };
 
-const applyHslToHex = (h: number, s: number, l: number): string => {
+const hslToHex = (h: number, s: number, l: number): string => {
     const [r, g, b] = hslToRgb(h, s, l).map((x) => Math.floor(x));
-    return fullColorHex(r, g, b);
+    return rgbToHex(r, g, b);
 };
 
 const getColor = (featIn: string): string => {
     return featIn === "0" ? "#38A800"
-                          : applyHslToHex( Math.random()
-                                         , (0.75 * Math.random()) + 0.25
-                                         , (0.25 * Math.random()) + 0.4
-                                         );
+                          : hslToHex( Math.random()
+                                    , (0.75 * Math.random()) + 0.25
+                                    , (0.25 * Math.random()) + 0.4
+                                    );
 };
 
 // via https://gis.stackexchange.com/questions/243136/geojson-add-and-format-line-features-to-a-leaflet-map
