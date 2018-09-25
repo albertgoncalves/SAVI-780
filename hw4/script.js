34e771e9e312e1632fb52f686e175f164cce4557
albertgoncalves <goncalves.albert@gmail.com>
2018-09-24 21:00:15 -0400

    Split things up a little bit.

--- hw4/script.js
+++ hw4/script.js
@@ -1,45 +1,4 @@
 // $ tslint script.ts ; tsc script.ts
-// via https://gist.github.com/mjackson/5311256
-var hslToRgb = function (h, s, l) {
-    var hslHelper = function (hh, ss, ll) {
-        var q = ll < 0.5 ? ll * (1 + ss)
-            : ll + ss - ll * ss;
-        var p = 2 * ll - q;
-        var rgb = [[p, q, hh + (1 / 3)] // r
-            ,
-            [p, q, hh] // g
-            ,
-            [p, q, hh - (1 / 3)] // b
-        ];
-        return rgb.map(function (x) { return hueToRgb(x) * 255; });
-    };
-    var hueToRgb = function (pqt) {
-        var p = pqt[0], q = pqt[1], t = pqt[2];
-        var tt = t < 0 ? t + 1
-            : t > 1 ? t - 1
-                : t;
-        return tt < (1 / 6) ? p + (q - p) * 6 * tt
-            : tt < (1 / 2) ? q
-                : tt < (2 / 3) ? p + (q - p) * ((2 / 3) - tt) * 6
-                    : p;
-    };
-    return s === 0 ? [l, l, l]
-        : hslHelper(h, s, l);
-};
-var rgbToHex = function (r, g, b) {
-    var rgbHelper = function (rgb) {
-        var hex = Number(rgb).toString(16);
-        return hex.length < 2 ? "0" + hex
-            : hex;
-    };
-    var _a = [r, g, b].map(function (x) { return rgbHelper(x); }), red = _a[0], green = _a[1], blue = _a[2];
-    return "#" + red + green + blue;
-};
-// 0 <= h, s, l <= 1
-var hslToHex = function (h, s, l) {
-    var _a = hslToRgb(h, s, l).map(function (x) { return Math.floor(x); }), r = _a[0], g = _a[1], b = _a[2];
-    return rgbToHex(r, g, b);
-};
 // via https://gis.stackexchange.com/questions/243136/geojson-add-and-format-line-features-to-a-leaflet-map
 var loadData = function (mapVar, url) {
     var getResp = function (response) { return response.json(); };
--- hw4/script.ts
+++ hw4/script.ts
@@ -1,54 +1,7 @@
 // $ tslint script.ts ; tsc script.ts
 
-declare var L: any;
-
-// via https://gist.github.com/mjackson/5311256
-
-const hslToRgb = (h: number, s: number, l: number): number[] => {
-
-    const hslHelper = (hh: number, ss: number, ll: number): number[] => {
-        const q = ll < 0.5 ? ll * (1 + ss)
-                           : ll + ss - ll * ss;
-        const p = 2 * ll - q;
-        const rgb: number[][] = [ [p, q, hh + (1 / 3)] // r
-                                , [p, q, hh          ] // g
-                                , [p, q, hh - (1 / 3)] // b
-                                ];
-        return rgb.map((x) => hueToRgb(x) * 255);
-    };
-
-    const hueToRgb = (pqt: number[]): number => {
-        const [p, q, t] = pqt;
-        const tt = t < 0 ? t + 1
-                 : t > 1 ? t - 1
-                         : t;
-        return tt < (1 / 6) ? p + (q - p) * 6 * tt
-             : tt < (1 / 2) ? q
-             : tt < (2 / 3) ? p + (q - p) * ((2 / 3) - tt) * 6
-                            : p;
-    };
-
-    return s === 0 ? [l, l, l]
-                   : hslHelper(h, s, l);
-};
-
-const rgbToHex = (r: number, g: number, b: number): string => {
-
-    const rgbHelper = (rgb: number): string => {
-        const  hex = Number(rgb).toString(16);
-        return hex.length < 2 ? "0" + hex
-                              : hex;
-    };
-
-    const [red, green, blue] = [r, g, b].map((x) => rgbHelper(x));
-    return "#" + red + green + blue;
-};
-
-// 0 <= h, s, l <= 1
-const hslToHex = (h: number, s: number, l: number): string => {
-    const [r, g, b] = hslToRgb(h, s, l).map((x) => Math.floor(x));
-    return rgbToHex(r, g, b);
-};
+declare var L       : any;
+declare var hslToHex: any;
 
 // via https://gis.stackexchange.com/questions/243136/geojson-add-and-format-line-features-to-a-leaflet-map
 
