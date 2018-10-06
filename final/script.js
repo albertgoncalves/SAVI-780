cffe47019b144e165b88f6ef94c077aa2ac256be
albertgoncalves <goncalves.albert@gmail.com>
2018-10-06 00:46:49 -0400

    Some progress.

--- final/script.js
+++ final/script.js
@@ -1,25 +1,84 @@
-/* global lines */
-/* global sttns */
-
-/* jshint esversion: 6 */
-/* jshint -W014 */
-
-const getField  = (field)      => (station)   => station[field];
-const checkLine = (line)       => (station)   => contains(station.lines)(line);
-const contains  = (mainString) => (subString) => {
+// tslint script.ts ; tsc script.ts
+//
+// shared utility functions
+//
+var contains = function (mainString) { return function (subString) {
     return mainString.indexOf(subString) >= 0 ? true
-                                              : false;
+        : false;
+}; };
+var dashes = function (str) { return "-" + str + "-"; };
+var checkField = function (searchTerm, field) {
+    return function (row) {
+        var column = row.properties[field];
+        return contains(column)(dashes(searchTerm));
+    };
 };
-
-// stations = [ {'name': '1', 'lines': 'A-C-E'}
-//            , {'name': '2', 'lines': 'C-D'  }
-//            , {'name': '3', 'lines': '7-D'  }
-//            , {'name': '4', 'lines': 'G-4'  }
-//            , {'name': '5', 'lines': 'Q-N-R'}
-//            , {'name': '6', 'lines': 'R-4-5'}
-//            , {'name': '7', 'lines': '4-5'  }
-//            , {'name': '8', 'lines': 'A-R-Q'}
-//            , {'name': '9', 'lines': 'G'    }
-//            ];
-
-console.log(sttns.filter(checkLine('G')).map(getField('name')));
+//
+// station search pattern
+//
+var search = function (featureArray, field) {
+    return function (searchTerm) {
+        return featureArray.filter(checkField(searchTerm, field));
+    };
+};
+var sttnsG = search(sttns.features, "line")("G");
+var sttnsGR = search(sttnsG, "line")("R"); // further reduce
+var sttnsGRF = search(sttnsGR, "line")("F"); // selected stops
+//
+// line search pattern
+//
+var splitSearch = function (featureArray, field) {
+    return function (searchTerm) {
+        var take = [];
+        var drop = [];
+        for (var _i = 0, featureArray_1 = featureArray; _i < featureArray_1.length; _i++) {
+            var row = featureArray_1[_i];
+            checkField(searchTerm, field)(row) ? take.push(row)
+                : drop.push(row);
+        }
+        return { take: take, drop: drop };
+    };
+};
+// no need to search
+var linesG = splitSearch(lines.features, "name")("G"); // matched rows since
+var linesR = splitSearch(linesG.drop, "name")("R"); // they are already
+var linesF = splitSearch(linesR.drop, "name")("F"); // on the map!
+//
+// test map
+//
+var loadData = function (mapVar, mapLayer) {
+    if (mapLayer === void 0) { mapLayer = null; }
+    return function (dataVar) {
+        var _ = mapLayer !== null === true ? mapLayer.clearLayers()
+            : null;
+        var mapData = L.geoJson(dataVar);
+        mapLayer = mapData.addTo(mapVar);
+        map.fitBounds(mapData.getBounds());
+        return mapLayer;
+    };
+};
+var tileUrl = ("https://stamen-tiles.a.ssl.fastly.net/toner/"
+    + "{z}/{x}/{y}.png");
+var origin = [40.7128,
+    -74.0060
+];
+var mapOpt = { doubleClickZoom: false,
+    dragging: false,
+    keyboard: false,
+    scrollWheelZoom: false,
+    tap: false,
+    touchZoom: false,
+    zoomControl: false
+};
+var map = L.map("map", mapOpt).setView(origin, 10);
+L.tileLayer(tileUrl).addTo(map);
+var pointsLayer = null; // initialize points layers ...
+// points need to be cleared after each reduction
+pointsLayer = loadData(map, pointsLayer)(sttnsG);
+loadData(map)(linesG.take);
+loadData(map)(linesR.take); // mapLayer variable can be ignored ...
+// lines, via search pattern, will never overlap
+setTimeout(function () {
+    pointsLayer = loadData(map, pointsLayer)(sttnsGRF);
+    loadData(map)(linesF.take);
+}, 3000);
---
+++ final/script.ts
@@ -0,0 +1,115 @@
+// tslint script.ts ; tsc script.ts
+
+/* global lines */
+/* global sttns */
+
+declare var L     : any;
+declare var lines : any;
+declare var sttns : any;
+
+interface Row     { properties: object; // not much type safety here...
+                  }
+
+interface Splits  { take: Row[];
+                    drop: Row[];
+                  }
+
+interface MapOpts { doubleClickZoom: boolean;
+                    dragging       : boolean;
+                    keyboard       : boolean;
+                    scrollWheelZoom: boolean;
+                    tap            : boolean;
+                    touchZoom      : boolean;
+                    zoomControl    : boolean;
+                  }
+
+//
+// shared utility functions
+//
+const contains = (mainString: string) => (subString: string): boolean => {
+    return mainString.indexOf(subString) >= 0 ? true
+                                              : false;
+};
+const dashes = (str: string): string => `-${str}-`;
+const checkField = (searchTerm: string, field: string) =>
+                   (row: Row): boolean => {
+    const column: string = row.properties[field];
+    return contains(column)(dashes(searchTerm));
+};
+
+//
+// station search pattern
+//
+const search = (featureArray: Row[], field: string) =>
+               (searchTerm: string): Row[] => {
+    return featureArray.filter(checkField(searchTerm, field));
+};
+
+const sttnsG   = search(sttns.features, "line")("G");
+const sttnsGR  = search(sttnsG        , "line")("R"); // further reduce
+const sttnsGRF = search(sttnsGR       , "line")("F"); // selected stops
+
+//
+// line search pattern
+//
+const splitSearch = (featureArray: Row[], field: string) =>
+                    (searchTerm: string): Splits => {
+    const take = [] as Row[];
+    const drop = [] as Row[];
+    for (const row of featureArray) {
+        checkField(searchTerm, field)(row) ? take.push(row)
+                                           : drop.push(row);
+    }
+    return {take, drop};
+};
+                                                         // no need to search
+const linesG = splitSearch(lines.features, "name")("G"); // matched rows since
+const linesR = splitSearch(linesG.drop   , "name")("R"); // they are already
+const linesF = splitSearch(linesR.drop   , "name")("F"); // on the map!
+
+//
+// test map
+//
+const loadData = (mapVar, mapLayer = null) => (dataVar) => {
+    const _ = mapLayer !== null === true ? mapLayer.clearLayers()
+                                         : null;
+
+    const mapData = L.geoJson(dataVar);
+    mapLayer = mapData.addTo(mapVar);
+    map.fitBounds(mapData.getBounds());
+
+    return mapLayer;
+};
+
+const tileUrl: string   = ( "https://stamen-tiles.a.ssl.fastly.net/toner/"
+                          + "{z}/{x}/{y}.png"
+                          );
+const origin : number[] = [  40.7128
+                          , -74.0060
+                          ];
+const mapOpt : MapOpts  = { doubleClickZoom: false
+                          , dragging       : false
+                          , keyboard       : false
+                          , scrollWheelZoom: false
+                          , tap            : false
+                          , touchZoom      : false
+                          , zoomControl    : false
+                          };
+
+const map = L.map("map", mapOpt).setView(origin, 10);
+L.tileLayer(tileUrl).addTo(map);
+
+let pointsLayer = null; // initialize points layers ...
+                        // points need to be cleared after each reduction
+
+pointsLayer = loadData(map, pointsLayer)(sttnsG);
+loadData(map)(linesG.take);
+loadData(map)(linesR.take); // mapLayer variable can be ignored ...
+                            // lines, via search pattern, will never overlap
+
+setTimeout(
+    () => {
+        pointsLayer = loadData(map, pointsLayer)(sttnsGRF);
+        loadData(map)(linesF.take);
+    }, 3000
+);
