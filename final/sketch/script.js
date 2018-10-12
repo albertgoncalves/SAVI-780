b66e888edfc798f72997366869607415c4217f4c
albertgoncalves <goncalves.albert@gmail.com>
2018-10-11 21:21:36 -0400

    Getting there...

--- final/sketch/script.js
+++ final/sketch/script.js
@@ -10,29 +10,39 @@ var createSvg = (containerId) => (svgShape) => (attributes, text="") => {
 
     document.getElementById(containerId).appendChild(newSvg);
 };
-
 var changeColor = (color) => (id) => () => {
     document.getElementById(id).style.color = color;
 };
+var arrayToString = (array) => array.map((item) => item.toString());
+var createCircleAttributes = (id, r, x, y) => {
+    var [rr, xx, yy] = arrayToString([r, x, y]);
+    return([ ["id", id]
+           , [ "r", rr]
+           , ["cx", xx]
+           , ["cy", yy]
+           ]
+          );
+};
+var createTextAttributes = (id, x, y) => {
+    var [xx, yy] = arrayToString([x, y]);
+    return([ ["id", id]
+           , [ "x", xx]
+           , [ "y", yy]
+           ]
+          );
+};
 
-circleAttributes = [ [ "r",        "40"]
-                   , ["cx",       "400"]
-                   , ["cy",        "30"]
-                   , ["id", "newCircle"]
-                   ];
-textAttributes   = [ ["class", "newText"]
-                   , [    "x",     "400"]
-                   , [    "y",      "30"]
-                   ];
+circleAttributes = createCircleAttributes("newCircle", 40, 400, 30);
+textAttributes   = createTextAttributes("newText", 400, 30);
 
 createSvg("circles")("circle")(circleAttributes);
 createSvg("circles")("text"  )(textAttributes, "Hello again!");
 
 ["circle", "newCircle"].forEach(
-    (circleId) => {
+    (elemId) => {
         document.querySelector(".button")
                 .addEventListener( "click"
-                                 , changeColor("hsl(350, 35%, 50%)")(circleId)
+                                 , changeColor("hsl(350, 35%, 50%)")(elemId)
                                  );
     }
 );
