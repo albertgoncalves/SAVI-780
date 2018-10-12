c23673b149ec2eaa14d9d8fb4ca37727fbe450d3
albertgoncalves <goncalves.albert@gmail.com>
2018-10-12 08:18:47 -0400

    Quick additions.

--- final/sketch/script.js
+++ final/sketch/script.js
@@ -2,25 +2,7 @@
 /* jshint -W014 */
 
 var arrayToString = (array) => array.map((item) => item.toString());
-var circleAttributes = (id, x, y, r) => {
-    var [xx, yy, rr] = arrayToString([x, y, r]);
-    return([ ["id", id]
-           , ["cx", xx]
-           , ["cy", yy]
-           , [ "r", rr]
-           ]
-          );
-};
-var textAttributes = (id, x, y) => {
-    var [xx, yy] = arrayToString([x, y]);
-    return([ ["id", id]
-           , [ "x", xx]
-           , [ "y", yy]
-           , ["text-anchor", "middle"]
-           , ["alignment-baseline", "middle"]
-           ]
-          );
-};
+
 var createSvg = (containerId) => (svgShape) => (attributes, text="") => {
     var newSvg = document.createElementNS( "http://www.w3.org/2000/svg"
                                          , svgShape
@@ -30,12 +12,37 @@ var createSvg = (containerId) => (svgShape) => (attributes, text="") => {
 
     document.getElementById(containerId).appendChild(newSvg);
 };
+
 var createCircle = (containerId) => (r) => (x, y) => (id) => {
+
+    var circleAttributes = (id, x, y, r) => {
+        var [xx, yy, rr] = arrayToString([x, y, r]);
+        return([ ["id", id]
+               , ["cx", xx]
+               , ["cy", yy]
+               , [ "r", rr]
+               ]
+              );
+    };
+
     createSvg(containerId)("circle")(circleAttributes(id, x, y, r));
 };
 var createText = (containerId) => (x, y) => (text) => (id) => {
+
+    var textAttributes = (id, x, y) => {
+        var [xx, yy] = arrayToString([x, y]);
+        return([ ["id", id]
+               , [ "x", xx]
+               , [ "y", yy]
+               , ["text-anchor", "middle"]
+               , ["alignment-baseline", "middle"]
+               ]
+              );
+    };
+
     createSvg(containerId)("text")(textAttributes(id, x, y), text);
 }
+
 var changeColor = (color) => (id) => () => {
     document.getElementById(id).style.color = color;
 };
@@ -46,11 +53,13 @@ var changeColor = (color) => (id) => () => {
 createCircle("circles")(40)(400, 30)("newCircle");
 createText("circles")(400, 30)("Hello again!")("newText");
 
-["circle", "newCircle"].forEach(
-    (elemId) => {
+[["circle", "230"], ["newCircle", "340"]].forEach(
+    ([elemId, newH]) => {
+        var newColor = `hsl(${newH}, 35%, 50%)`
+
         document.querySelector(".button")
                 .addEventListener( "click"
-                                 , changeColor("hsl(350, 35%, 50%)")(elemId)
+                                 , changeColor(newColor)(elemId)
                                  );
     }
 );
